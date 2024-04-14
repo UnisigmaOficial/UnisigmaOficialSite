7ec310e7b7d18721cf24adcb9e8552b4351c5ed9
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-04-14 20:11:48 -0300

    create Presentation section

---
+++ src/app/components/presentation/presentation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentation',
+  standalone: true,
+  imports: [],
+  templateUrl: './presentation.component.html',
+  styleUrl: './presentation.component.css'
+})
+export class PresentationComponent {
+
+}
