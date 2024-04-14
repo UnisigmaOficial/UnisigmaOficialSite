b2df222426c3cc4cb5d0c597d17b310d1fc5c2cf
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-04-14 20:11:48 -0300

    create Presentation section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { PresentationComponent } from './components/presentation/presentation.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PresentationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
