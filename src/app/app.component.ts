9458d1da605bbaca2a647dd105654664a0e38f38
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-04-14 21:40:10 -0300

    create Services section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { PresentationComponent } from './components/presentation/presentation.component';
+import { OurServicesComponent } from './components/our-services/our-services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PresentationComponent],
+  imports: [RouterOutlet, PresentationComponent, OurServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
