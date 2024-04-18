6a3e71689be05aa3a33c4e587738a850f1e95211
Jesye Pereira <jessyekessia16@gmail.com>
2024-04-18 17:17:49 -0300

    pages adicionadas

---
+++ src/app/pages/portifolio/portifolio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portifolio',
+  standalone: true,
+  imports: [],
+  templateUrl: './portifolio.component.html',
+  styleUrl: './portifolio.component.css'
+})
+export class PortifolioComponent {
+
+}
