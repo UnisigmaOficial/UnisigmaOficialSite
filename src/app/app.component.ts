13128f43ae5adf08bbe8ce35b62d91ed4304652d
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-04-18 15:33:17 -0300

    fix component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { PresentationComponent } from './components/presentation/presentation.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { WhoweareComponent } from './whoweare/whoweare.component';
-import { ValuesComponent } from './values/values.component';
-import { FeedbacksComponent } from './feedbacks/feedbacks.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WhoweareComponent } from './components/whoweare/whoweare.component';
+import { ValuesComponent } from './components/values/values.component';
+import { FeedbacksComponent } from './components/feedbacks/feedbacks.component';
 import { OurServicesComponent } from './components/our-services/our-services.component';
 import { ContactFormsComponent } from './components/contact-forms/contact-forms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PresentationComponent, OurServicesComponent, ContactFormsComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, PresentationComponent, OurServicesComponent, ContactFormsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
