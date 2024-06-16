3b9b8d62565c5c8a5a30512db8f88fe5833830b2
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-06-16 12:07:32 -0300

    remove content from Portfolio page

--- src/app/pages/portifolio/portifolio.component.ts
+++ src/app/pages/portifolio/portifolio.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { WhoweareComponent } from '../../components/whoweare/whoweare.component';
-import { ValuesComponent } from '../../components/values/values.component';
-import { FeedbacksComponent } from '../../components/feedbacks/feedbacks.component';
 
 @Component({
   selector: 'app-portifolio',
   standalone: true,
-  imports: [WhoweareComponent, ValuesComponent, FeedbacksComponent],
+  imports: [],
   templateUrl: './portifolio.component.html',
   styleUrl: './portifolio.component.css'
 })
