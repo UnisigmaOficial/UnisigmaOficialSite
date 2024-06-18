6ebbc5e5752f5c12da145d7f1d513e8c2f470885
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-06-18 12:43:09 -0300

    fix menu closing when navigating links

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -13,4 +13,9 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 })
 export class HeaderComponent {
   menuIcon = faBars;
+  @ViewChild('menuCheckbox') menuCheckbox!: ElementRef<HTMLInputElement>;
+
+  closeMenu() {
+    this.menuCheckbox.nativeElement.checked = false;
+  }
 }
