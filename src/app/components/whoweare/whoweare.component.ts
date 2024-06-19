5960b5a4e30da2af327c14ba4354723ef892836e
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-06-19 12:45:24 -0300

    fix broken link

--- src/app/components/whoweare/whoweare.component.ts
+++ src/app/components/whoweare/whoweare.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-whoweare',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './whoweare.component.html',
   styleUrl: './whoweare.component.css'
 })
