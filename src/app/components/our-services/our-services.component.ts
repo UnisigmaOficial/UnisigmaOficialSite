2574211761db5f5c6d9fc1fe0bbb674dff640d44
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-07-20 20:25:09 -0300

    add new services

--- src/app/components/our-services/our-services.component.ts
+++ src/app/components/our-services/our-services.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-
 import { RouterLink } from '@angular/router';
 
-import { ServiceCardComponent } from '../service-card/service-card.component';
 import { Service } from '../service-card/service';
+import { ServiceCardComponent } from '../service-card/service-card.component';
 
 @Component({
   selector: 'app-our-services',
@@ -16,43 +15,53 @@ export class OurServicesComponent {
   services: Service[] = [
     {
       title: 'Marketing',
-      text: 'Plano de ação para o endomarketing e indomarketing, além de plano de marketing para as redes sociais',
-      image: '/assets/undraw_marketing_re_7f1g 1.svg'
+      text: 'Plano de ação para o endomarketing e indomarketing, além de plano de marketing para as redes sociais.',
+      image: '/assets/services-illustrations/undraw_solution_mindset_re_57bf 1.svg'
     },
     {
       title: 'Estruturação Financeira',
-      text: 'Análise das entradas e saídas, planejamento financeiro, implementação de fluxo de caixa, treinamento e acompanhamento',
-      image: '/assets/undraw_financial_data_re_p0fl 1.svg'
+      text: 'Análise das entradas e saídas, planejamento financeiro, implementação de fluxo de caixa, treinamento e acompanhamento.',
+      image: '/assets/services-illustrations/undraw_financial_data_re_p0fl 1.svg'
     },
     {
       title: 'Planejamento Estratégico',
-      text: 'Análise dos dados operacionais e dos sistemas utilizados pela empresa, para sugerir melhorias e adaptações, atualização do fluxo operacional, treinamento e acompanhamento',
-      image: '/assets/undraw_business_decisions_re_84ag 1.svg'
+      text: 'Análise dos dados operacionais e dos sistemas utilizados pela empresa, para sugerir melhorias e adaptações, atualização do fluxo operacional, treinamento e acompanhamento.',
+      image: '/assets/services-illustrations/undraw_business_decisions_re_84ag 1.svg'
     },
     {
-      title: 'Pesquisa de mercado',
+      title: 'Pesquisa de Mercado',
       text: 'Consiste na análise dos concorrentes físicos e digitais, além da análise dos leads e dos produtos.',
-      image: '/assets/undraw_learning_sketching_nd4f 1.svg'
+      image: '/assets/services-illustrations/undraw_learning_sketching_nd4f 1.svg'
     },
     {
-      title: 'E-commerce',
-      text: 'Levantamento de dados, listagem de pré-requisitos, desenvolvimento, treinamento e entrega',
-      image: '/assets/undraw_progressive_app_m-9-ms 1.svg'
-    },
-    {
-      title: 'Desenvolvimento de sites',
-      text: 'Em um mundo cada vez mais moderno, ter presença na internet através de um website proporciona visibilidade, acessibilidade e credibilidade para as empresas.',
-      image: '/assets/undraw_static_website_re_x70h 1.svg'
+      title: 'Cliente Oculto',
+      text: 'Avaliação da experiência de compra dos clientes. Identificar pontos a serem melhorados na jornada do cliente, para que sua empresa ofereça cada vez mais qualidade.',
+      image: '/assets/services-illustrations/undraw_solution_mindset_re_57bf 1.svg'
     },
     {
       title: 'Customer Experience',
       text: 'Mapear, analisar e identificar pontos de melhora na jornada do cliente, a análise é totalmente pautada na forma como os clientes percebem e se relacionam com a marca.',
-      image: '/assets/undraw_experience_design_re_dmqq 1.svg'
+      image: '/assets/services-illustrations/undraw_experience_design_re_dmqq 1.svg'
     },
     {
-      title: 'Prototipagem e Design',
-      text: 'Desenvolver e aplicar técnicas de prototipagem para visualizar e testar diferentes conceitos de design, centrado na criação de modelos interativos que validam e refinam ideias de forma eficaz.',
-      image: '/assets/undraw_design_tools_-42-tf 1.svg'
+      title: 'Desenvolvimento de Sites',
+      text: 'Em um mundo cada vez mais moderno, ter presença na internet através de um website proporciona visibilidade, acessibilidade e credibilidade para as empresas.',
+      image: '/assets/services-illustrations/undraw_static_website_re_x70h 1.svg'
+    },
+    {
+      title: 'Sistema de Agendamento',
+      text: 'Produzimos sistema de agendamento de serviços que permite empresas e profissionais organizem suas consultas, agendamentos e compromissos de maneira eficiente.',
+      image: '/assets/services-illustrations/undraw_online_calendar_re_wk3t 1.svg'
+    },
+    {
+      title: 'Sistema de Gestão',
+      text: 'Ferramenta abrangente para empresas que precisam gerenciar suas operações de serviço de forma eficaz Pode incluir rastreamento de pedidos, gerenciamento de inventário, controle financeiro e etc.',
+      image: '/assets/services-illustrations/undraw_hiring_re_yk5n 1.svg'
+    },
+    {
+      title: 'Sistema de Menu Digital',
+      text: 'Oferecemos um sistema de cardápio digital inovador para restaurantes e bares, permitindo que seus clientes acessem o menu e façam pedidos diretamente através de QR codes ou pelo whatsApp.',
+      image: '/assets/services-illustrations/undraw_diet_ghvw 1.svg'
     },
   ]
 }
