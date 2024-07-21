e46c149a0d2e6093516160fc9b4dfd5685f30193
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-07-21 09:59:06 -0300

    update images

--- src/app/components/our-team/our-team.component.ts
+++ src/app/components/our-team/our-team.component.ts
@@ -16,79 +16,73 @@ export class OurTeamComponent {
       name: 'Gisele Silva',
       office: 'Presidente Executiva',
       linkedinUrl: 'https://www.linkedin.com/in/gisele-silva-69308718a/',
-      photo: './assets/gisele.png'
+      photo: '/assets/team-photos/gisele.png'
     },
     {
       name: 'Sophia Tomei',
       office: 'Diretor de Admin-fin',
       linkedinUrl: 'https://www.linkedin.com/in/sophia-tomei-1a9716278/',
-      photo: './assets/sophia.png'
+      photo: '/assets/team-photos/sophia.png'
     },
     {
       name: 'Mylena Macena',
       office: 'Gerente de Marketing',
       linkedinUrl: 'https://www.linkedin.com/in/mylena-macena-747482232/',
-      photo: './assets/mylena.png'
-    },
-    {
-      name: 'Daniel Felizardo',
-      office: 'Diretor de Marketing',
-      linkedinUrl: 'https://www.linkedin.com/in/danielgfelizardo/',
-      photo: './assets/daniel.png'
+      photo: '/assets/team-photos/mylena.png'
     },
     {
       name: 'Lucas Andrade',
       office: 'Desenvolvedor',
       linkedinUrl: 'https://www.linkedin.com/in/lucasandrade912/',
-      photo: './assets/lucas.png'
+      photo: '/assets/team-photos/lucas.png'
     },
     {
       name: 'Jessye Pereira',
       office: 'Desenvolvedora',
       linkedinUrl: 'https://www.linkedin.com/in/jessyepereira/',
-      photo: './assets/jessye.png'
+      photo: '/assets/team-photos/jessye.png'
     },
     {
       name: 'Johnner Yelcde',
       office: 'Desenvolvedor',
       linkedinUrl: 'https://www.linkedin.com/in/johnner-yelcde-8b2736253/',
-      photo: './assets/johnner.png'
+      photo: '/assets/team-photos/johnner.png'
     },
     {
       name: 'Jonata Barbosa',
       office: 'Desenvolvedor',
       linkedinUrl: 'https://www.linkedin.com/in/jonata-barbosa-31103a19b/',
-      photo: './assets/jon.png'
+      photo: '/assets/team-photos/jon.png'
     },
     {
       name: 'Caue Trajano',
       office: 'Desenvolvedor',
       linkedinUrl: 'https://www.linkedin.com/in/cauetrajano/',
-      photo: './assets/caue.png'
+      photo: '/assets/team-photos/caue.png'
     },
     {
       name: 'Maira Fernandes',
       office: 'Desenvolvedora',
       linkedinUrl: 'https://www.linkedin.com/in/maira-fernandes-130761273/',
-      photo: './assets/maira.png'
+      photo: '/assets/team-photos/maira.png'
     },
     {
       name: 'Matheus Kauã',
       office: 'Consultor de Adm-fin',
       linkedinUrl: 'https://www.linkedin.com/in/matheus-souza-a8520a249/',
-      photo: './assets/matheus.png'
+      photo: '/assets/team-photos/matheus.png'
     },
     {
       name: 'Thaysa Mary',
       office: 'Consultor de Adm-fin',
       linkedinUrl: 'https://www.linkedin.com/in/thaysa-mary-354849209/',
-      photo: './assets/thaysa.png'
+      photo: '/assets/team-photos/thaysa.png'
     },
     {
       name: 'Mateus Douglas',
       office: 'Gente e Gestão',
       linkedinUrl: 'https://www.linkedin.com/in/mateus-albuquerque-ab220a270/',
-      photo: './assets/douglas.png'
+      photo: '/assets/team-photos/douglas.png'
     },
   ]
 }
